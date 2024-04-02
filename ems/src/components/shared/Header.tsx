68f6384d7f9b4690fa1ddcf9053deb0c273f5e92
Hans-ByondXR <hans@byondxr.com>
2024-04-02 16:29:34 +0800

    supabase integration with basic auth form

--- ems/src/components/shared/Header.tsx
+++ ems/src/components/shared/Header.tsx
@@ -1,7 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { Button } from "../ui/button";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="max-w-7xl lg:mx-auto p-5 md:px-10 xl:px-0 w-full flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image
+            src="/public/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="VibeVault logo"
+          />
+        </Link>
+        <div className="flex w-32 justify-end gap-3">
+          <Button className="rounded-full" size={"lg"}>
+            <Link href="/sign-in">Login</Link>
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
